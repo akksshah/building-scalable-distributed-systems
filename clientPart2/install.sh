c8cde6a614b525f5c6aff8ea5ba46cf058b4f0cb
Aakash Shah <aakashshahd@gmail.com>
2021-03-31 10:52:50 -0400

    Fix tests

--- clientPart2/install.sh
+++ clientPart2/install.sh
@@ -1,6 +1,6 @@
 if test -n "$1"; then
-    echo "hello $1"
     cd ~/Desktop/bsds-clean/building-scalable-distributed-systems/clientPart2/ || exit
+    echo "==============================================================================="
     echo "PACKAGING JAR FILE FOR DEPLOY"
     mvn package
     cd ../../
@@ -8,7 +8,7 @@ if test -n "$1"; then
     ssh-keyscan -H "$1" >> ~/.ssh/known_hosts
     echo "We assume that there is a directory named aakash already installed in the ec2"
     scp -i "aakash6650.pem" building-scalable-distributed-systems/clientPart2/out/artifacts/clientPart2_jar/clientPart2.jar ec2-user@"$1":aakash/client.jar
-    echo "yes"
+    echo "run this >> java -jar aakash/client.jar"
     ssh -i "aakash6650.pem" ec2-user@"$1"
 else
   echo "USAGE ./install.sh <ec2 domain>"
--- clientPart2/src/test/java/TestApi.java
+++ clientPart2/src/test/java/TestApi.java
@@ -1,5 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.Properties;
 import java.util.concurrent.ThreadLocalRandom;
 
 import io.swagger.client.ApiException;
@@ -8,58 +10,41 @@ import io.swagger.client.model.Purchase;
 import io.swagger.client.model.PurchaseItems;
 import lombok.var;
 
+import static org.junit.Assert.fail;
+
 public class TestApi {
-     private static final String serverIpAddress
-             = "52.87.235.220:8080";
-//private static final String serverIpAddress = "3.91.241.111";
-//    private static final String serverIpAddress = "localhost";
-    private static final String serverPort = "8080";
     private static PurchaseApi api;
-    private static final int numPurchases = 1;
-    private static final int numberOfHoursOperational = 1;
-    private static final int numberOfItemsToPurchasePerOrder = 1;
     private static final int storeId = 1;
     private static final int numberOfCustomerPerStore = 1;
     private static final int maxItemId = 1000;
-    private static final List<RequestTracker> requestTracker = new ArrayList<>();
-    public static void main(String[] args) {
+    private static String serverIpAddress;
+
+    public static void main(String[] args) throws ApiException {
         var purchase = new Purchase();
         var itemsToPurchase = new PurchaseItems();
         itemsToPurchase.setNumberOfItems(5);
-        itemsToPurchase.setItemID(String.valueOf(ThreadLocalRandom.current().nextInt(1, maxItemId)));
+        itemsToPurchase.setItemID(String.valueOf(ThreadLocalRandom.current()
+                                                         .nextInt(1, maxItemId)));
         purchase.addItemsItem(itemsToPurchase);
-        var requestInfoTracker = new RequestTracker();
-        try {
-            requestInfoTracker.setRequestType(RequestType.POST);
-            requestInfoTracker.setStartTime(System.currentTimeMillis());
-            var response = getApi().newPurchaseWithHttpInfo(
+        var response = getApi().newPurchaseWithHttpInfo(
                 purchase,
                 storeId,
-                ThreadLocalRandom.current().nextInt(storeId * 1000, storeId * 1000 + numberOfCustomerPerStore),
+                ThreadLocalRandom.current()
+                        .nextInt(storeId * 1000, storeId * 1000 + numberOfCustomerPerStore),
                 "20200101");
-            requestInfoTracker.setResponseCode(response.getStatusCode());
-            if (response.getStatusCode() == 200 || response.getStatusCode() == 201) {
-                System.out.println("Success");
-                //counter.requestSucceeded();
-                // System.out.println(counter.getTotalRequestSent() + " Request sent, this was successful");
-            } else {
-                System.out.println("Failure");
-                //counter.requestFailed();
-                System.err.println("Request failed" + response);
-            }
-        } catch (ApiException e) {
-            System.err.println("Request failed due to: " + e.getMessage());
-            requestInfoTracker.setResponseCode(null);
-            // counter.requestFailed();
-        } finally {
-            requestInfoTracker.setEndTime(System.currentTimeMillis());
+        if (response.getStatusCode() == 200 || response.getStatusCode() == 201) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+            System.err.println("Request failed" + response);
+            throw new RuntimeException("Request failed to execute");
         }
     }
+
     private static String getUrl() {
-        return "http://52.87.235.220:8080/deployEc2/";
-//        return serverIpAddress.equals("localhost")
-//                   ? "http://localhost:8080/deployEc2_war_exploded/"
-//                   : "http://" + serverIpAddress + "/deployEc2/";
+        return serverIpAddress.equals("localhost")
+               ? "http://localhost:8080/deployEc2_war_exploded/"
+               : "http://" + serverIpAddress + "/deployEc2/";
     }
 
     private static PurchaseApi getApi() {
@@ -68,5 +53,29 @@ public class TestApi {
             api.getApiClient().setBasePath(getUrl());
         }
         return api;
+    }
+
+    @Test
+    public void runCheck() {
+        try {
+            var properties = new Properties();
+            var propertiesFileName = "config.properties";
+            var inputStream =
+                    ClientApp.class.getClassLoader().getResourceAsStream(propertiesFileName);
+            if (inputStream == null) {
+                throw new FileNotFoundException("Property file: " + propertiesFileName + " does " +
+                                                        "not exist");
+            } else {
+                properties.load(inputStream);
+            }
+            serverIpAddress = properties.getProperty("serverIpAddress") == null
+                              ? "bsds-1992801789.us-east-1.elb.amazonaws.com"
+                              : properties.getProperty("serverIpAddress");
+            System.out.println("Hitting on: " + getUrl());
+            main(new String[]{});
+        } catch (Exception e) {
+            System.err.println("FAILURE REASON: " + e.getMessage());
+            fail();
+        }
     }
 }
